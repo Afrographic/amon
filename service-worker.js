6a72378f18bece05ba834d75a6bba99ca5268d41
Afrographix <bruneltesse@gmail.com>
2025-08-13 13:26:49 +0100

    service worker updated

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "amon-v1";
-const ASSETS = [
+const CACHE_NAME = "amonV1";
+const ASSETS_TO_CACHE  = [
     "/",
     '/index.html',
     '/manifest.json',
@@ -93,40 +93,32 @@ const ASSETS = [
 
 ]
 
-self.addEventListener("install",
-    event=>{
-        event.waitUntil(
-            caches.open(CACHE_NAME).then(
-                cache =>{
-                    return cache.addAll(ASSETS);
-                }
-            )
-        )
-    }
-)
+// sw.js
 
-self.addEventListener("activate",
-    event => {
-        event.waitUntil(
-            caches.keys().then(
-                keys => Promise.all(
-                    keys.filter( k => k !== CACHE_NAME).map(
-                        k => caches.delete(k)
-                    )
-                )
-            )
-        )
-    }
-)
 
-self.addEventListener("fetch",event => {
-    event.respondWith(
-        caches.match(event.request).then(
-            event.respondWith(
-                caches.match(event.request).then(
-                    response => response || fetch(event.request)
-                )
-            )
-        )
-    )
-})
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request))
+  );
+});
+
