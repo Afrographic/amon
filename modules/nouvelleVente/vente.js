24098e0420e47cef8038894617d159a6bf678e72
Afrographix <bruneltesse@gmail.com>
2025-09-16 09:59:17 +0100

    Vente de plusieurs produits en cours

--- modules/nouvelleVente/vente.js
+++ modules/nouvelleVente/vente.js
@@ -1,55 +1,104 @@
-
 class NouvelleVente {
   static productsList = document.querySelector(".productsList");
+  static searchTokenInput = document.querySelector("#searchToken");
+  static searchButton = document.querySelector("#searchButton");
+  static closeSearch = document.querySelector("#closeSearch");
+
   static devise = localStorage.getItem("amonDevise");
+
   static products = [];
+  static searchedProducts = [];
+  static searching = false;
 
   static async init() {
     this.getDataFromAmonDB()
-    .then(async (records) => {
-      NouvelleVente.formatProducts(JSON.parse(records[0].products))
-    })
-    .catch((err) => console.error("Error getting data:", err));
+      .then(async (records) => {
+        NouvelleVente.formatProducts(JSON.parse(records[0].products));
+      })
+      .catch((err) => console.error("Error getting data:", err));
   }
 
-  static formatProducts(products){
-    for(let i = 0 ; i<=products.length-1;i++){
-        products[i].selected = false;
-        products[i].qteToBuy = 0;
+  static formatProducts(products) {
+    for (let i = 0; i <= products.length - 1; i++) {
+      products[i].selected = false;
+      products[i].qteToBuy = 0;
     }
     NouvelleVente.products = products;
     NouvelleVente.renderProduct();
-    console.log( NouvelleVente.products);
+    console.log(NouvelleVente.products);
   }
 
   static renderProduct() {
     this.productsList.innerHTML = "";
-    for(let i = 0 ; i<= NouvelleVente.products.length-1;i++){
-        let id = NouvelleVente.products[i].id;
-        let selected = NouvelleVente.products[i].selected ? "active" :"";
-        this.productsList.innerHTML += `
-        <div class="productItem">
-            <div class="productName" onclick="NouvelleVente.selectProduct('${id}')">
-                <div class="selector ${selected}"></div>
-                <div>${NouvelleVente.products[i].nom} (${Afro.formatNumWithWhiteSpace(NouvelleVente.products[i].prixVente)} ${this.devise})</div>
-            </div>
-            <div class="quantity">
-                <button>-</button>
-                <div class="qteValue">0</div>
-                <button>+</button>
-            </div>
-        </div>
-        `
+    for (let i = 0; i <= NouvelleVente.products.length - 1; i++) {
+      this.productsList.innerHTML += NouvelleVente.buildTemplate(
+        NouvelleVente.products[i]
+      );
     }
   }
 
-  static selectProduct(id){
-    for(let i = 0 ;i <= NouvelleVente.products.length - 1; i++){
-        if(NouvelleVente.products[i].id == id){
-            NouvelleVente.products[i].selected = !NouvelleVente.products[i].selected;
+  static buildTemplate(product) {
+    let id = product.id;
+    let selected = product.selected ? "active" : "";
+    let inactive = product.qteToBuy == 0 ? "inactive" :"";
+    return `
+        <div class="productItem">
+            <div class="productName" onclick="NouvelleVente.selectProduct('${id}')">
+                <div class="selector ${selected}"></div>
+                <div>${product.nom} (${Afro.formatNumWithWhiteSpace(
+      product.prixVente
+    )} ${this.devise})</div>
+            </div>
+            <div class="quantity ${inactive}">
+                <button onclick="NouvelleVente.decrease('${id}')">-</button>
+                <div class="qteValue">${product.qteToBuy}</div>
+                <button onclick="NouvelleVente.increase('${id}')">+</button>
+            </div>
+        </div>
+        `;
+  }
+
+  static decrease(id) {
+    for (let i = 0; i <= NouvelleVente.products.length - 1; i++) {
+      if (NouvelleVente.products[i].id == id) {
+        NouvelleVente.products[i].qteToBuy--;
+        if (NouvelleVente.products[i].qteToBuy < 0) {
+          NouvelleVente.products[i].qteToBuy = 0;
         }
+      }
     }
     NouvelleVente.renderProduct();
+  }
+
+  static increase(id) {
+    for (let i = 0; i <= NouvelleVente.products.length - 1; i++) {
+      if (NouvelleVente.products[i].id == id) {
+        NouvelleVente.products[i].qteToBuy++;
+        if (
+          NouvelleVente.products[i].qteToBuy >
+          NouvelleVente.products[i].quantite
+        ) {
+          NouvelleVente.products[i].qteToBuy =
+            NouvelleVente.products[i].quantite;
+        }
+      }
+    }
+    NouvelleVente.renderProduct();
+  }
+
+  static selectProduct(id) {
+    for (let i = 0; i <= NouvelleVente.products.length - 1; i++) {
+      if (NouvelleVente.products[i].id == id) {
+        NouvelleVente.products[i].qteToBuy = 0;
+        NouvelleVente.products[i].selected =
+          !NouvelleVente.products[i].selected;
+          if( NouvelleVente.products[i].selected){
+            NouvelleVente.products[i].qteToBuy++;
+          }
+      }
+    }
+    NouvelleVente.renderProduct();
+    NouvelleVente.closeSearchView()
   }
 
   static getDataFromAmonDB() {
@@ -76,6 +125,41 @@ class NouvelleVente {
         reject(`IndexedDB error: ${event.target.error}`);
       };
     });
+  }
+
+  static searchProduct() {
+    let token = this.searchTokenInput.value;
+    if (token.trim().length == 0) {
+      this.renderProduct();
+      this.searching = false;
+      return;
+    }
+    this.searching = true;
+    this.searchButton.classList.add("inactive");
+    this.closeSearch.classList.remove("inactive");
+    this.searchedProducts = [];
+    for (let i = 0; i <= this.products.length - 1; i++) {
+      if (this.products[i].nom.toLowerCase().includes(token.toLowerCase())) {
+        this.searchedProducts.push(this.products[i]);
+      }
+    }
+    NouvelleVente.renderSearchTemplate();
+  }
+
+  static renderSearchTemplate(){
+    this.productsList.innerHTML = `<p>${NouvelleVente.searchedProducts.length} Resultat(s)</p>`;
+    for (let i = 0; i <= NouvelleVente.searchedProducts.length - 1; i++) {
+      this.productsList.innerHTML += NouvelleVente.buildTemplate(
+        NouvelleVente.searchedProducts[i]
+      );
+    }
+  }
+
+  static closeSearchView() {
+    this.searchButton.classList.remove("inactive");
+    this.closeSearch.classList.add("inactive");
+    this.searchTokenInput.value = "";
+    this.renderProduct();
   }
 }
 
