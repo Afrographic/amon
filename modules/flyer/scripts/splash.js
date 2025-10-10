91a951e3232b3de4d0cc2f3763a3cfe56239af21
Afrographix <bruneltesse@gmail.com>
2025-10-10 08:52:46 +0100

    New model

--- modules/flyer/scripts/splash.js
+++ modules/flyer/scripts/splash.js
@@ -1,7 +1,7 @@
 class Splash{
     static splash = document.querySelector("#splash");
     static async hide_splash(){
-        await Tools.sleep(1);
+        await Tools.sleep(2300);
         this.splash.classList.add("hidden")
     }
 }
