db31a547f7533bb81ed28337355d91255c9f3fd9
Afrographix <bruneltesse@gmail.com>
2026-07-17 11:00:35 +0100

    lidav deploy

--- modules/lidav/flutter_bootstrap.js
+++ modules/lidav/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2783898665" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1964643014" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
