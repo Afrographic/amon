cbb05f7eb67aea5b31a46040524b848006ed3394
Afrographix <bruneltesse@gmail.com>
2026-08-06 18:55:01 +0100

    new deploy

---
+++ modules/lidav/sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('push', function(event) {
+
+    const data = event.data.json();
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, {
+            body: data.body,
+            icon: '/icons/Icon-192.png'
+        })
+    );
+
+});
