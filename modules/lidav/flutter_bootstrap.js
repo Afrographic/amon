862581692703eb177b2280789e35966b9fb6138e
Afrographix <bruneltesse@gmail.com>
2026-08-13 05:11:36 +0100

    added audio on course

--- modules/lidav/flutter_bootstrap.js
+++ modules/lidav/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1231090358" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1508756380" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
