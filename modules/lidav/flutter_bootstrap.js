e25b9aaa38ecf82a74815a5811a8d0cd61cafe1e
Tesse Brunel <bruneltesse@gmail.com>
2026-08-31 19:11:43 +0100

    update

--- modules/lidav/flutter_bootstrap.js
+++ modules/lidav/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3812437985" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "783095970" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
